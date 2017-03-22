85c5d8fadc443c812fdade2b1530b2891789a117
Tim Schindler <tim.schindler@gmail.com>
2017-03-22 19:02:07 -0400

    refactor and moved code to modify_published_date

--- generate.py
+++ generate.py
@@ -9,6 +9,7 @@ from feedgen.feed import FeedGenerator
 ESPN_RSS_FEED = 'http://espn.go.com/espnradio/feeds/rss/podcast.xml?id=9941853'
 CONTACT = {'name': 'Tim Schindler',
            'email': 'tim.schindler@gmail.com'}
+FEED_TIMEZONE = 'America/Los_Angeles'
 
 
 def episode_duration_string_to_int(duration):
@@ -17,6 +18,23 @@ def episode_duration_string_to_int(duration):
     else:
         ftr = [60, 1]
     return sum([a * b for a, b in zip(ftr, [int(i) for i in duration.split(":")])])
+
+def modify_published_date(episode, entry):
+    tz = pytz.timezone(FEED_TIMEZONE)
+
+    dt = datetime.fromtimestamp(time.mktime(episode.published_parsed))
+    date = tz.localize(dt)
+
+    if 'Show: ' in episode.title:
+        entry.published(date)
+    elif 'Hour 1' in episode.title:
+        entry.published(date + timedelta(hours=1))
+    elif 'Hour 2' in episode.title:
+        entry.published(date + timedelta(hours=2))
+    elif 'Hour 3' in episode.title:
+        entry.published(date + timedelta(hours=3))
+    else:
+        entry.published(date + timedelta(hours=-1))
 
 def generate_feed(output_file, exclude_highlights=True):
     # Parse RSS feed
@@ -43,8 +61,6 @@ def generate_feed(output_file, exclude_highlights=True):
     fg.podcast.itunes_explicit(itunes_explicit='clean')
     fg.podcast.itunes_owner(name=CONTACT['name'], email=CONTACT['email'])
 
-    tz = pytz.timezone('America/Los_Angeles')
-
     for e in d.entries:
 
         if exclude_highlights and episode_duration_string_to_int(e['itunes_duration']) > 3600:
@@ -63,20 +79,7 @@ def generate_feed(output_file, exclude_highlights=True):
             fe.podcast.itunes_subtitle(e.description)
             fe.podcast.itunes_duration(e['itunes_duration'])
 
-            dt = datetime.fromtimestamp(time.mktime(e.published_parsed))
-            date = tz.localize(dt)
-
-            # Local hour
-            if 'Show: ' in e.title:
-                fe.published(date)
-            elif 'Hour 1' in e.title:
-                fe.published(date + timedelta(hours=1))
-            elif 'Hour 2' in e.title:
-                fe.published(date + timedelta(hours=2))
-            elif 'Hour 3' in e.title:
-                fe.published(date + timedelta(hours=3))
-            else:
-                fe.published(date + timedelta(hours=-1))
+            modify_published_date(e, fe)
 
     fg.rss_str(pretty=True)
     fg.rss_file(output_file)
